00da309eeaa6da1cf3af4138a46e4e2996b5e2f7
Tyler Long <tyler4long@gmail.com>
2019-04-25 10:53:59 +0800

    Fix responseCard issue

--- AmazonLexGenerator/ResponseCard.cs
+++
@@ -1,9 +0,0 @@
-namespace AmazonLexGenerator
-{
-    public class ResponseCard
-    {
-        public int version = 1;
-        public string contentType = "application/vnd.amazonaws.card.generic";
-        public string[] genericAttachments = new string[] { };
-    }
-}
