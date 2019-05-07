9ad5142dec2489ea6dd1583f93597e04cc0a85d6
Tyler Long <tyler4long@gmail.com>
2019-05-07 14:21:29 -0700

    Fix EditDndStatusIntent issue

--- AmazonLexGenerator.Demo.RcAssistant/Intents/EditDndStatusIntent.cs
+++ AmazonLexGenerator.Demo.RcAssistant/Intents/EditDndStatusIntent.cs
@@ -7,6 +7,7 @@ namespace AmazonLexGenerator.Demo.RcAssistant
         public EditDndStatusIntent() : base("EditDndStatusIntent", null)
         {
             sampleUtterances = new[] {"edit", "change", "update", "set", "alter", "modify"}
+                .Cartesian(new[] {"", "my"})
                 .Cartesian(new[] {"DnD status", "do not disturb status"})
                 .Cartesian(new[] {"to {dndStatusType}", ""})
                 .Concat(new[] {"{dndStatusType}", "{dndStatusType} right now"}).ToArray();
--- AmazonLexGenerator.Demo.RcAssistant/SlotTypes/DndStatusType.cs
+++ AmazonLexGenerator.Demo.RcAssistant/SlotTypes/DndStatusType.cs
@@ -20,7 +20,7 @@ namespace AmazonLexGenerator.Demo.RcAssistant
                     .Cartesian(new[] {"department calls"}).Concat(new[] {"no department calls"}).ToArray()),
             };
             promptMessage =
-                "Do you want to set your Do Not Disturb to **Take All Calls**, **Do Not Accept Any Calls**, **Take Department Calls Only** or **Do Not Accept Any Calls**?";
+                "Do you want to set your Do Not Disturb to **Take All Calls**, **Do Not Accept Any Calls**, **Take Department Calls Only** or **Do Not Accept Department Calls**?";
             sampleUtterances = new[] {"edit", "change", "update", "set", "alter", "modify"}
                 .Cartesian(new[] {"it to {dndStatusType}"}).Take(10).ToArray();
         }
